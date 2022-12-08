1ecb8fac0b7571340e60a7535eeeb75bf3c64307
Fablaney <fabrmoto@gmail.com>
2022-12-08 14:11:26 +0100

    commit

--- Billed-app-FR-Front-origine/src/views/LoginUI.js
+++ Billed-app-FR-Front-origine/src/views/LoginUI.js
@@ -12,36 +12,41 @@ export default () => {
             <h1> Billed </h1>
           </div>
         </div>
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <form class="form-signin" data-testid="form-employee">
-                  <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
-                  <label for="inputEmail">Votre email</label>
-                  <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
-                  <label for="inputPassword">Mot de passe</label>
-                  <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
-                  <button class="btn btn-lg btn-primary btn-block" data-testid="employee-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
-                </form>
+
+          <div class="row">
+
+              <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <form class="form-signin" data-testid="form-employee">
+                      <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
+                      <label for="inputEmail">Votre email</label>
+                      <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                      <label for="inputPassword">Mot de passe</label>
+                      <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
+                      <button class="btn btn-lg btn-primary btn-block" data-testid="employee-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
+                    </form>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <form class="form-signin" data-testid="form-admin">
-                  <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
-                  <label for="inputEmail">Votre email</label>
-                  <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
-                  <label for="inputPassword">Mot de passe</label>
-                  <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
-                  <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
-                </form>
+
+              <div class="col-sm-6">
+                  <div class="card">
+                      <div class="card-body">
+                          <form class="form-signin" data-testid="form-admin">
+                              <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
+                              <label for="inputEmail">Votre email</label>
+                              <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                              <label for="inputPassword">Mot de passe</label>
+                              <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
+                              <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
+                          </form>
+                      </div>
+                  </div>
               </div>
-            </div>
+
           </div>
-        </div>
+
       </div>
   `)
 }
